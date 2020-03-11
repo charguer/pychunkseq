508d6028c35b81f425dd48b14ad706895aea825c
Marco Nassabain <marco.nassabain@hotmail.com>
2020-03-11 16:37:35 +0100

    implementé seq et seq.push_front, WIP seq.print

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import chunk
 import pchunk
 import pstack
 import view
+import seq
 
 def main():
     # mychunk = chunk.chunk()
@@ -21,20 +22,20 @@ def main():
     # print('\nmychunk2')
     # mychunk2.print_general(print)   # [2, 3]
 
-    mychunk = pchunk.Pchunk()
-    mychunk = mychunk.push_right(3)
-    mychunk = mychunk.push_left(2)
-    mychunk2 = mychunk.push_left(1)
-    mychunk = mychunk.push_left(4)
-    print('mychunk')
-    mychunk.print_general(print_item)    # [4, 2, 3]
-    print('\nmychunk2')
-    mychunk2.print_general(print_item)   # [1, 2, 3]
+    # mychunk = pchunk.Pchunk()
+    # mychunk = mychunk.push_right(3)
+    # mychunk = mychunk.push_left(2)
+    # mychunk2 = mychunk.push_left(1)
+    # mychunk = mychunk.push_left(4)
+    # print('mychunk')
+    # mychunk.print_general(print_item)    # [4, 2, 3]
+    # print('\nmychunk2')
+    # mychunk2.print_general(print_item)   # [1, 2, 3]
 
-    mychunk, four  = mychunk.pop_left()
-    mychunk, three = mychunk.pop_right()
-    print('\nmychunk, enlevé', four , 'à gauche et', three, 'à droite')
-    mychunk.print_general(print_item)    # [2]
+    # mychunk, four  = mychunk.pop_left()
+    # mychunk, three = mychunk.pop_right()
+    # print('\nmychunk, enlevé', four , 'à gauche et', three, 'à droite')
+    # mychunk.print_general(print_item)    # [2]
 
     # mystack = pstack.Pstack()
     # mystack = mystack.push(1)
@@ -65,6 +66,11 @@ def main():
     # mychunk.pop_left()
     # print("get relatif 1er elt = ", mychunk.get(0))
     # print("get absolu 1er elt = ", mychunk.get_absolute(0))
+
+    myseq = seq.Seq()
+    for i in range(25):
+        myseq.push_front(i)
+    myseq.print_general(print_item)
 
 def print_item(item):
     print(item, end="")
---
+++ seq.py
@@ -0,0 +1,43 @@
+import chunk
+
+class Seq:
+    
+    def __init__(self):
+        self.front = chunk.chunk()
+        self.back = chunk.chunk()
+        self.middle = None # appel au constructeur donne boucle infinie
+        # self.free_front = chunk.chunk()
+        # self.free_back = chunk.chunk()
+
+    def is_empty(self):
+        return self.front.is_empty() and self.back.is_empty() and (self.middle is None or self.middle.is_empty())
+
+    def push_front(self, item):
+        if self.front.is_full():
+            if self.back.is_empty():
+                if (self.middle is not None and not self.middle.is_empty()):
+                    # erreur: conditions non respectées
+                    return self
+                else:
+                    f = self.front
+                    self.front = self.back
+                    self.back = f
+                    # self.front.push_left(item)
+            else:
+                if self.middle is None:
+                    self.middle = Seq()
+                self.middle.push_front(self.front)
+                self.front = chunk.chunk() # TODO: utiliser free_front
+        self.front.push_left(item)
+
+    # TODO: fix print si on est dans middle
+    def print_general(self, print_item):
+        print("front = ", end="")
+        self.front.print_general(print_item)
+        if self.middle is None:
+            print("middle = None")
+        else:
+            print("middle =", end="")
+            self.middle.print_general(chunk.chunk.print_general)
+        print("back = ", end="")
+        self.back.print_general(print_item)
