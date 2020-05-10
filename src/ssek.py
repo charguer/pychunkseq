7c8d136d0daafc857ce219fd18a369ba543f030f
charguer <arthur@chargueraud.org>
2020-05-10 22:14:05 +0200

    comment ssek

--- src/ssek.py
+++ src/ssek.py
@@ -10,7 +10,8 @@ def create(pov, this, middle, that, version_max):
 
 def create_and_populate(pov, this, middle, that, version_max):
     if this.is_empty() and middle is not None and not middle.is_empty():
-        middle, this = middle.pop(pov, version_max)
+        middle, this = middle.pop(pov, version_max) 
+        # TODO par symmétrie, on pourrait écrire pov ^ FRONT
     if that.is_empty() and middle is not None and not middle.is_empty():
         middle, that = middle.pop(pov ^ BACK, version_max)
     return create(pov, this, middle, that, version_max)
@@ -18,6 +19,10 @@ def create_and_populate(pov, this, middle, that, version_max):
 
 class Ssek:
 
+    # TODO: en fait tu peux prendre version_max = NO_VERSION = -1, car au début tu
+    # n'auras que des chunks supports de version -1, donc ça marche.
+
+    # TODO: comme dans Esek, peut être ça serait plus propre de couper cette fonction en deux.
     def __init__(self, front = None, middle = None, back = None, version_max = 0):
         self.version_max = version_max
         self.front = schunk.create(version_max) if front == None else front
@@ -47,31 +52,46 @@ class Ssek:
             self.front = that
             self.back = this
 
+    # TODO: pour toutes les fonctions ci-dessous qui prennent version en argument, 
+    # mettre version=NO_VERSION comme argument par défaut.
+
     def push(self, pov, item, version):
         this, that = self.get_both(pov)
         if this.is_full():
             if that.is_empty():
                 assert self.middle is None or self.middle.is_empty()
                 new_this = that.push(pov, item, version)
+                # TODO: faire pour clarifier : new_that = this
+                # TODO: tu peux utiliser : create(pov, new_this, None, new_that, self.version_max)
                 new_ssek = Ssek()   # TODO: avoid allocating if replacing
                 new_ssek.set_both(pov, new_this, this)
             else:
                 new_this = schunk.create(version)
                 new_this = new_this.push(pov, item, version)
+                # TODO: ce n'est pas un bug, mais il faut mieux éviter de modifier self en place.
+                # Faire plutôt:
+                #    middle = self.middle 
+                #    if middle is None:
+                #         middle = Ssek()
                 if self.middle is None:
                     self.middle = Ssek()
+                # ici tu enchaînerais avec un middle.push, et pas un self.middle.push
                 new_middle = self.middle.push(pov, this, version)
+                # TODO: pour la fin, utiliser : create(pov, new_this, middle, that, self.version_max)
                 new_ssek = Ssek()   # TODO: avoid allocating if replacing
                 new_ssek.set_both(pov, new_this, that)
                 new_ssek.middle = new_middle
         else:
             new_this = this.push(pov, item, version)
+            # TODO: le code ci-dessous alloue des chunks, pourtant ce n'est pas utile,
+            # et en plus il y a un bug car tu ne vas pas conserver le version_max
+            # alors que pourtant tu conserves le middle et tous ses chunks.
+            # Solution, je pense : create(pov, new_this, self.middle, that, self.version_max)
             new_ssek = Ssek()
             new_ssek.set_both(pov, new_this, that)
             new_ssek.middle = self.middle
         return new_ssek
 
-    
     def pop(self, pov, version):
         assert not self.is_empty()
         this, that = self.get_both(pov)
